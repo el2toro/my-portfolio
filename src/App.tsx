768a4232df9009a86b137390f1dbccc0d010defa
el2toro <vasea.badragan@gmail.com>
2023-05-30 15:12:21 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
+import { motion } from "framer-motion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreen = useMediaQuery('(min-width: 1060px)')
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) setIsTopOfPage(true)
+      if(window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return window.removeEventListener("scroll", handleScroll)
+  }, []);
+
+   return (
+    <div className="app bg-deep-blue">
+      <Navbar isTopOfPage={isTopOfPage} 
+              selectedPage={selectedPage} 
+              setSelectedPage={setSelectedPage}
+      />
+
+      <div className="w5/6 mx-auto md:h-full">
+        {isAboveMediumScreen && (
+          <DotGroup selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage} />
+        )}
+        <motion.div
+          
+          onViewportEnter={() => setSelectedPage("home")}
         >
-          Learn React
-        </a>
-      </header>
+          <Landing setSelectedPage={setSelectedPage} />
+        </motion.div>
+      </div>
     </div>
   );
 }
