c9c595df3dbb4739f418677deee910dd597303d5
el2toro <vasea.badragan@gmail.com>
2024-05-24 10:44:51 +0200

    Add scroll to top functionality

--- src/app/core/components/shell/shell.component.ts
+++ src/app/core/components/shell/shell.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT, ViewportScroller } from '@angular/common';
+import { Component, HostListener, OnInit, inject } from '@angular/core';
+import { Observable, fromEvent, map } from 'rxjs';
 
 @Component({
   selector: 'app-shell',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shell.component.scss']
 })
 export class ShellComponent implements OnInit {
-
+  scrollPosition = false;
+  private readonly document = inject(DOCUMENT);
+  private readonly viewport = inject(ViewportScroller);
+  
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() {
+    this.scrollPosition = this.viewport.getScrollPosition()?.[1] > 0;
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
+ 
+
+  readonly showScroll$: Observable<boolean> = fromEvent(
+    this.document,
+    'scroll'
+  ).pipe(
+    map(() => this.viewport.getScrollPosition()?.[1] > 0)
+  );
+
+  onScrollToTop(): void {
+    this.viewport.scrollToPosition([0, 0]);
+  }
 }
