aa7ac52e9c9aab40d115cbf46eb60beae1e59dcd
el2toro <vasea.badragan@gmail.com>
2025-01-20 15:23:29 +0100

    Clean

--- src/app/features/feature-portfolio/pages/home-page/home-page.component.ts
+++ src/app/features/feature-portfolio/pages/home-page/home-page.component.ts
@@ -1,5 +1,4 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,26 +6,7 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  @ViewChild('statistic') statistic!: ElementRef;
-
-  @HostListener('window:scroll', [])
-  public onViewportScroll(){
-    // ⤵️ Captures / defines current window height when called
-const windowHeight = window.innerHeight;
-// ⤵️ Captures bounding rectangle of 5th element
-const boundingRectheroSecrion = this.statistic.nativeElement.getBoundingClientRect();
-// ⤵️ Captures bounding rectangle of 8th element
-//const boundingRectEight = this.divEight.nativeElement.getBoundingClientRect();
-
-//console.log('windowHeight: ', windowHeight)
-//console.log('boundingRectFive: ', boundingRectheroSecrion)
-const isInView = boundingRectheroSecrion.top >= 0 && boundingRectheroSecrion.top <= window.innerHeight;
-
-if(isInView){
- // console.log('Is in view OK')
-}
-
-  }
+  
   constructor() { }
 
   ngOnInit() {
