2c9dbdc410ac0c266df793ccab2ef028787480a3
el2toro <vasea.badragan@gmail.com>
2025-04-05 23:15:21 +0200

    move firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,8 @@ import { getAnalytics } from "firebase/analytics";
 })
 export class AppComponent {
   title = 'my-portfolio';
+ // firebaseConfig = environment.firebaseConfig;
 
-//  firebaseConfig = {
-//     apiKey: "AIzaSyD7_-PoKCO8n1swMTTGSkU1Ou5M-L1nYVo",
-//     authDomain: "my-portfolio-001-1.firebaseapp.com",
-//     projectId: "my-portfolio-001-1",
-//     storageBucket: "my-portfolio-001-1.appspot.com",
-//     messagingSenderId: "262894568047",
-//     appId: "1:262894568047:web:434c050f92afe612c6439a",
-//     measurementId: "G-GLQZH05WNT"
-//   };
 
   constructor(private matIconRegistry: MatIconRegistry,
               private domSanitizer: DomSanitizer
@@ -28,16 +21,16 @@ export class AppComponent {
     this.matIconRegistry.addSvgIcon('menu', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/menu-icon.svg'));
     this.matIconRegistry.addSvgIcon('close', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/close-icon.svg'));
 
-    this.matIconRegistry.addSvgIcon('programmer-laptop', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/programmer-laptop.svg'));
-    this.matIconRegistry.addSvgIcon('radio-button-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/radio-button-icon.svg'));
-    this.matIconRegistry.addSvgIcon('partner-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/partner-icon.svg'));
-    this.matIconRegistry.addSvgIcon('spark-ornament-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/spark-ornament-icon.svg'));
-    this.matIconRegistry.addSvgIcon('magic-wand-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/magic-wand-icon.svg'));
-    this.matIconRegistry.addSvgIcon('phone-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/phone-icon.svg'));
-    this.matIconRegistry.addSvgIcon('email-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/email-icon.svg'));
-    this.matIconRegistry.addSvgIcon('address-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/address-icon.svg'));
-    this.matIconRegistry.addSvgIcon('button-arrow-right', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/button-arrow-right.svg'));
-    this.matIconRegistry.addSvgIcon('outlined-dot', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/outlined-dot.svg'));
+    this.matIconRegistry.addSvgIcon('programmer-laptop', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/programmer-laptop.svg'));
+    this.matIconRegistry.addSvgIcon('radio-button-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/radio-button-icon.svg'));
+    this.matIconRegistry.addSvgIcon('partner-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/partner-icon.svg'));
+    this.matIconRegistry.addSvgIcon('spark-ornament-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/spark-ornament-icon.svg'));
+    this.matIconRegistry.addSvgIcon('magic-wand-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/magic-wand-icon.svg'));
+    this.matIconRegistry.addSvgIcon('phone-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/phone-icon.svg'));
+    this.matIconRegistry.addSvgIcon('email-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/email-icon.svg'));
+    this.matIconRegistry.addSvgIcon('address-icon', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/address-icon.svg'));
+    this.matIconRegistry.addSvgIcon('button-arrow-right', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/button-arrow-right.svg'));
+    this.matIconRegistry.addSvgIcon('outlined-dot', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/outlined-dot.svg'));
 
     //Social Media
     this.matIconRegistry.addSvgIcon('facebook', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/facebook-icon.svg'));
@@ -48,9 +41,9 @@ export class AppComponent {
 
 
     //Arrows
-    this.matIconRegistry.addSvgIcon('chevron-right', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/chevron-rigth.svg'));
-    this.matIconRegistry.addSvgIcon('chevron-left', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/chevron-left.svg'));
-    this.matIconRegistry.addSvgIcon('arrow-right', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/arrow-email-button.svg'));
+    this.matIconRegistry.addSvgIcon('chevron-right', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/chevron-rigth.svg'));
+    this.matIconRegistry.addSvgIcon('chevron-left', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/chevron-left.svg'));
+    this.matIconRegistry.addSvgIcon('arrow-right', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/arrow-email-button.svg'));
     this.matIconRegistry.addSvgIcon('arrow-basic-right', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/svg/arrow-basic-right.svg'));
 
     // Initialize Firebase
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,16 @@ export const environment = {
     serviceId: 'service_8pl9d5a',
     templateId: 'template_1uw198v',
     userId: '_0Pf3Ua69CSlBp7rr'
+  },
+
+  firebaseConfig: {
+    apiKey: "AIzaSyD7_-PoKCO8n1swMTTGSkU1Ou5M-L1nYVo",
+    authDomain: "my-portfolio-001-1.firebaseapp.com",
+    projectId: "my-portfolio-001-1",
+    storageBucket: "my-portfolio-001-1.appspot.com",
+    messagingSenderId: "262894568047",
+    appId: "1:262894568047:web:434c050f92afe612c6439a",
+    measurementId: "G-GLQZH05WNT"
   }
 };
 
