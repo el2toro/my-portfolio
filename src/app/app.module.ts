ded9af2bee7a47410edc5e0e33a489d49657e71c
el2toro <vasea.badragan@gmail.com>
2025-11-01 16:35:34 +0100

    migrate angular to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgOptimizedImage, provideImgixLoader } from '@angular/common';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    NgOptimizedImage 
-  ],
-  providers: [
-    provideImgixLoader('http://localhost:4200/assets/images/')
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        CoreModule,
+        BrowserAnimationsModule,
+        NgOptimizedImage], providers: [
+        provideImgixLoader('http://localhost:4200/assets/images/'),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
